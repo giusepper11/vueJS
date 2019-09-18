bcbfa6d1ccea30b3a3ffb3e4ea169111e5d985b2
Giuseppe Rosa <giusepper11@gmail.com>
2019-09-18 15:25:52 -0300

    Finalizando a desafio de estilos

--- dom/desafio-04/app.js
+++ dom/desafio-04/app.js
@@ -1,28 +1,60 @@
 new Vue({
-	el: '#desafio',
-	data: {
-		css: 'Init'
-	},
-	methods: {
-		iniciarEfeito() {
-			setInterval(() => {
-				this.css = !this.css
-			}, 1000);
-		},
-		iniciarProgresso() {
+    el: '#desafio',
+    data: {
+        css: 'Init',
+        cssex21: 'classe1',
+        cssex22: 'classe2',
+        cssex3: '',
+        cssex41: '',
+        cssex42: '',
+        cssex5: '',
+        percent: 0,
+    },
+    methods: {
+        iniciarEfeito() {
+            setInterval(() => {
+                this.css = !this.css
+            }, 1000);
+        },
+        iniciarProgresso() {
+            setInterval(() => {
+                if (this.percent < 100) {
 
-		}
-	},
-	computed: {
-		efeito() {
-			if (this.css !== 'Init') {
-				return {
-					destaque: this.css,
-					encolher: !this.css
-				}
-			}
 
-		}
-	}
+                    this.percent += 10
+                }
+            }, 500)
+
+
+        },
+    },
+    computed: {
+        efeito() {
+            if (this.css !== 'Init') {
+                return {
+                    destaque: this.css,
+                    encolher: !this.css
+                }
+            }
+
+        },
+
+        ex4() {
+
+            return {
+                destaque: this.cssex41 === 'true'
+            }
+        },
+
+        progressBar() {
+            return {
+                backgroundColor: 'red',
+                height: '20 px', margin: '20px',
+                width: `${2*this.percent}px`
+            };
+        }
+
+    }
 
 })
+;
