e6ce55a1a3114294bc654592ca7ad0af6296e0d0
Giuseppe Rosa <giusepper11@gmail.com>
2019-09-20 13:33:26 -0300

    Finalizando projeto monster killer

--- p1-kill_monsters/app.js
+++ p1-kill_monsters/app.js
@@ -1,14 +1,57 @@
 new Vue({
     el: '#app',
     data: {
-        playerLife: 0,
+        running: false,
+        playerLife: 100,
         monsterLife: 100,
+        logs: [],
     },
     computed: {
         hasResult() {
             return this.playerLife === 0 || this.monsterLife === 0
         }
     },
-    methods: {},
-    watch: {}
+    methods: {
+        startGame() {
+            this.running = true;
+            this.playerLife = 100;
+            this.monsterLife = 100;
+            this.logs = []
+        },
+        attack(especial) {
+            this.hurt('monsterLife', 5, 10, especial, 'Jogador', 'Monstro', 'player');
+            if (this.monsterLife > 0) {
+                this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster');
+            }
+
+        },
+        hurt(atr, min, max, especial, source, target, classe) {
+            const plus = especial ? 5 : 0;
+            const hurt = this.getRandom(min + plus, max + plus);
+            this[atr] = Math.max(this.playerLife - hurt, 0); //garante q o valor nunca sera negativo
+            this.registerLog(`${source} atingiu ${target} com ${hurt}.`, classe)
+        },
+        healAndHurt() {
+            this.heal(10, 15);
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+        },
+        heal(min, max) {
+            const heal = this.getRandom(min, max);
+            this.playerLife = Math.min(this.playerLife + heal, 100);
+            this.registerLog(`Jogador ganhou força de ${heal}.`, 'player')
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min;
+            return Math.round(value)
+        },
+        registerLog(text, classe) {
+            this.logs.unshift({text, classe})
+        }
+    },
+    watch: {
+        hasResult(value) {
+            if (value) this.running = false
+
+        }
+    }
 });
