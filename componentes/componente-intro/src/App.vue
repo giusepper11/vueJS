2168ede5bd25a08e51f8097348c0e63edf096f63
Giuseppe Rosa <giusepper11@gmail.com>
2019-09-24 14:47:10 -0300

    Adicionando componentes de forma global

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="xpto">
+      <h1>Contadores</h1>
+      <Contador></Contador>
+      <Contador></Contador>
+      <Contador></Contador>
+      <Contador/>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
