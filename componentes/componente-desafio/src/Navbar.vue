b1274ac941e5430ac6ddcf53d6810fd884f38933
Giuseppe Rosa <giusepper11@gmail.com>
2019-09-24 16:17:52 -0300

    Finalizando o desafio de componentes, quebrando a aplicação em componentes

---
+++ componentes/componente-desafio/src/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-navigation-drawer app>
+        <Navtoolbar/>
+        <v-divider></v-divider>
+        <Menulist/>
+    </v-navigation-drawer>
+</template>
+
+<script>
+    import Navtoolbar from "./Navtoolbar";
+    import Menulist from "./Menulist";
+
+    export default {
+        name: "Navbar",
+        components: {
+            Navtoolbar,
+            Menulist,
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
