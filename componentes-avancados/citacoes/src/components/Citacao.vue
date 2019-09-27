6c2da7bf6f0a27725d9e43a65e128f4aafca2e0b
Giuseppe Rosa <giusepper11@gmail.com>
2019-09-27 11:18:37 -0300

    Trabalhando com SLOTS

---
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="citacao">
+        <!--        TAG VUE pra passar o conteudo montado onde chamou esse container-->
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+    h1 {
+        color: red !important;
+    }
+</style>
