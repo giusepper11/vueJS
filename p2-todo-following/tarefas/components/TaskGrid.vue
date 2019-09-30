2b855473f511574cf36261b5dbe236739ef6d832
Giuseppe Rosa <giusepper11@gmail.com>
2019-09-29 23:12:45 -0300

    Executrando projeto conforme instrução e adicionando evento de mudança de estado

---
+++ p2-todo-following/tarefas/components/TaskGrid.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="task-grid">
+        <template v-if="tasks.length">
+            <Task v-for="(task, id) in tasks" :key="task.name" :task="task"
+                  @taskDeleted="$emit('taskDeleted',id)"></Task>
+        </template>
+        <p v-else class="no-task">Sua vida está em dia</p>
+    </div>
+</template>
+
+<script>
+
+    import Task from "./Task";
+
+    export default {
+        name: "TaskGrid",
+        props: {
+            tasks: {type: Array, required: true}
+        },
+        components: {Task}
+    }
+</script>
+
+<style scoped>
+    .task-grid {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    .task-grid .task {
+        margin: 10px;
+    }
+
+    .no-task {
+        color: #aaa;
+        font-size: 1.2rem;
+    }
+</style>
---
+++ p2-todo-following/tarefas/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+    <div id="app">
+        <h1>Tarefas</h1>
+        <NewTask @taskAdded="addTask"/>
+        <TaskGrid @taskDeleted="deleteTask" :tasks="tasks"/>
+    </div>
+</template>
+
+<script>
+    import TaskGrid from "../components/TaskGrid";
+    import NewTask from "../components/NewTask";
+
+    export default {
+        components: {TaskGrid, NewTask},
+        data() {
+            return {
+                tasks: [
+                    {name: 'Lavar a louça', pending: false},
+                    {name: 'Comprar blusa', pending: true},
+                ]
+            }
+        },
+        methods: {
+            addTask(task) {
+                const sameNAme = t => t.name === task.name;
+                const reallyNew = this.tasks.filter(sameNAme).length === 0;
+                if (reallyNew) {
+                    this.tasks.push({
+                        name: task.name,
+                        pending: true
+                    })
+                }
+            },
+            deleteTask(i) {
+                this.tasks.splice(i, 1)
+            }
+        }
+    }
+</script>
+
+<style>
+    body {
+        font-family: 'Lato', sans-serif;
+        background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+        color: #FFF;
+    }
+
+    #app {
+        display: flex;
+        flex: 1;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+
+    #app h1 {
+        margin-bottom: 5px;
+        font-weight: 300;
+        font-size: 3rem;
+    }
+</style>
